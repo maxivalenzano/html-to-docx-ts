ae863cafd825f709c0f236644956fa9921abf82b
Jason Finch <jason.finch@gmail.com>
2022-03-06 09:13:43 +1000

    chore: trying stuff to compile

--- rollup.config.js
+++ rollup.config.js
@@ -7,16 +7,45 @@ import builtins from 'rollup-plugin-node-builtins';
 import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 // import typescript from '@rollup/plugin-typescript';
+import ts from 'rollup-plugin-ts';
 
 import * as meta from './package.json';
 
 const baseConfig = createBasicConfig();
 
+const globals = {
+  fs: 'fs',
+  path: 'path',
+  util: 'util',
+  events: 'events',
+  lodash: 'lodash',
+  'html-to-vdom': 'HTMLToVDOM',
+  'virtual-dom': 'virtual-dom',
+  'virtual-dom/vnode/is-vnode': 'virtual-dom',
+  'virtual-dom/vnode/vnode': 'VNode',
+  'virtual-dom/vnode/vtext': 'VText',
+};
+
 export default merge(baseConfig, {
-  input: './out-tsc/index.js',
-  external: ['color-name', 'escape-html', 'html-to-vdom', 'jszip', 'virtual-dom', 'xmlbuilder2'],
+  //input: './out-tsc/index.js',
+  input: './src/index.ts',
+  external: [
+    'color-name',
+    'escape-html',
+    'html-to-vdom',
+    'jszip',
+    'virtual-dom',
+    'virtual-dom/vnode/is-vnode',
+    'virtual-dom/vnode/is-vtext',
+    'virtual-dom/vnode/vnode',
+    'virtual-dom/vnode/vtext',
+    'xmlbuilder2',
+    'image-size',
+    'lodash',
+  ],
   plugins: [
     // typescript(),
+    ts(),
     resolve(),
     json({ include: 'package.json', preferConst: true }),
     commonjs(),
@@ -32,13 +61,7 @@ export default merge(baseConfig, {
     {
       file: 'dist/html-to-docx.esm.js',
       format: 'es',
-      globals: {
-        fs: 'fs',
-        path: 'path',
-        util: 'util',
-        events: 'events',
-        'html-to-vdom': 'HTMLToVDOM',
-      },
+      globals: globals,
       sourcemap: true,
       banner: `// ${meta.homepage} v${meta.version} Copyright ${new Date().getFullYear()} ${
         meta.author
@@ -52,13 +75,7 @@ export default merge(baseConfig, {
       banner: `// ${meta.homepage} v${meta.version} Copyright ${new Date().getFullYear()} ${
         meta.author
       }`,
-      globals: {
-        fs: 'fs',
-        path: 'path',
-        util: 'util',
-        events: 'events',
-        'html-to-vdom': 'HTMLToVDOM',
-      },
+      globals: globals,
     },
   ],
 });
