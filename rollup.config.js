5e52c9e053434f2c7ce0df9c55e807bcc3be9662
Jason Finch <jason.finch@gmail.com>
2022-03-06 22:43:41 +1000

    chore: re-enable terser

--- rollup.config.js
+++ rollup.config.js
@@ -24,14 +24,14 @@ export default {
   input: './src/index.ts',
   external: ['color-name', 'escape-html', 'html-to-vdom', 'jszip', 'virtual-dom', 'xmlbuilder2'],
   plugins: [
-    typescript({ tsconfig: './tsconfig.json' }), // config: https://github.com/rollup/plugins/tree/master/packages/typescript
+    typescript({ tsconfig: './tsconfig.json' }),
     resolve(), // locates modules using the Node resolution algorithm, for using third party modules in node_modules
     json({ include: 'package.json', preferConst: true }),
     commonjs({ extensions: ['.js', '.ts'] }), // import CommonJS files, see note about using with typescript --> https://github.com/rollup/plugins/tree/master/packages/typescript
     builtins(), // Allows the node builtins to be required/imported. Doing so gives the proper shims to support modules that were designed for Browserify, some modules require rollup-plugin-node-globals.
-    // terser({  //minifier
-    //   mangle: false,
-    // }),
+    terser({
+      mangle: false,
+    }),
     cleaner({
       targets: ['./dist/'],
     }),
